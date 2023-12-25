43b7b424955b20a0a9c382d7f29c636a612f862e
Henry Hiles <henry@henryhiles.com>
2023-12-25 11:50:00 -0500

    You can no longer go back in time

--- app/src/main/java/com/henryhiles/qweather/presentation/components/weather/WeatherForecast.kt
+++ app/src/main/java/com/henryhiles/qweather/presentation/components/weather/WeatherForecast.kt
@@ -3,7 +3,6 @@ package com.henryhiles.qweather.presentation.components.weather
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,15 +16,14 @@ fun WeatherForecast(
     onChangeSelected: (Int) -> Unit
 ) {
     state.hourlyWeatherInfo?.weatherData?.let {
-        val rowState = rememberLazyListState(LocalDateTime.now().hour)
-        LazyRow(state = rowState, modifier = modifier) {
-            itemsIndexed(it) { index, data ->
+        val hour = LocalDateTime.now().hour
+        LazyRow(modifier = modifier) {
+            itemsIndexed(it.subList(hour, it.size)) { index, data ->
                 WeatherHour(
                     data = data,
                     modifier = Modifier
-                        .padding(horizontal = 8.dp),
-                    onChangeSelected = { onChangeSelected(index) }
-                )
+                        .padding(horizontal = 8.dp)
+                ) { onChangeSelected(index) }
             }
         }
     }
