0f1c861aa7093f68b2cb29c3561b66b43ec1f0b4
Henry Hiles <henry@henryhiles.com>
2023-12-26 11:50:07 -0500

    Lower min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.henryhiles.qweather"
-        minSdk = 30
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
